ddafb63247178697b698499ec9c35eb3efec92cc
Sara <sarahosin26@gmail.com>
2024-05-27 19:01:42 +0200

    added user and todos

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todos")
+    name = models.CharField(max_length = 100)
+    description = models.TextField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
