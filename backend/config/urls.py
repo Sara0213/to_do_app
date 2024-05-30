bed73630b570957f10b5675cc1d81ae04ed595ad
Sara <sarahosin26@gmail.com>
2024-05-30 12:15:18 +0200

    added login and refresh token endpoints

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.api.urls'))
+    path('api/users/', include('users.api.urls')),
+    # widoki dla tokenow
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # logowanie
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh') # odnawianie tokena
 ]
